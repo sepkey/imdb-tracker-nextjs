2cca8b5ccaa92580aa38b60f252ede9c9a82d051
Sepide Kia <sepidekia6990@gmail.com>
2024-05-15 08:31:58 +0330

    add watched movies to local storage

---
+++ hooks/useLocalStorage.ts
@@ -0,0 +1,26 @@
+'use client';
+import { IWatchedMovie } from '@/types/types';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+
+type LocalStorageStateHook<T> = [T, Dispatch<SetStateAction<T>>];
+
+export function useLocalStorage(
+  initialState: IWatchedMovie[],
+  key: string
+): LocalStorageStateHook<IWatchedMovie[]> {
+  const [value, setValue] = useState<IWatchedMovie[]>(() => {
+    if (typeof window !== undefined) {
+      const storageValue = window.localStorage.getItem(key)!;
+      return storageValue ? JSON.parse(storageValue) : initialState;
+    }
+  });
+
+  useEffect(
+    function () {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    [key, value]
+  );
+
+  return [value, setValue];
+}
